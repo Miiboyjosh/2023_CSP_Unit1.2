f8feb84b75a33a8788d1c2c73a6a6e1f3cf32443
Joshua <joshua.jean@vtsd.com>
2023-11-14 07:58:15 -0500

    edsdhnfj

--- 1-2-1/a121_catch_a_turtle_[initials].py
+++ 1-2-1/a121_catch_a_turtle_[initials].py
@@ -1,13 +1,13 @@
 # a121_catch_a_turtle.py
 # -----import statements-----
 import turtle as trtl
-import random as randint
+import random as rand
 # -----game configuration----
 
 dot_color = "orange"
 dot_shapesize = 10
+dot_shape = "circle"
 dot_fillcolor = "orange"
-dot_startpos: (0, 0)
 
 
 
@@ -18,13 +18,16 @@ dot.pensize(dot_shapesize)
 dot.fillcolor(dot_fillcolor)
 
 # -----game functions--------
-f =
-dot.onclick(f)
-def spot_clicked(x, y):
-    print("Spot was clicked!")
-
+def dot_clicked(x, y):
+    change_position()
+def change_position():
+    rand.randint(400, 300)
+    new_xpos = rand.randint(400, 300)
+    new_ypos = rand.randint(400, 300)
+    dot.goto(new_xpos, new_ypos)
 
 # -----events----------------
+dot.onclick(dot_clicked)
 
 wn = trtl.Screen()
 wn.mainloop()
