53d4e26f67771546692217a8e9024239bd67ecfa
Joshua <joshua.jean@vtsd.com>
2023-11-16 09:24:11 -0500

    edsdhnfj

---
+++ 1-2-1/Challange.py
@@ -0,0 +1,15 @@
+x = "*"
+def triangle():
+    global x
+    for step in range(5):
+        print(x)
+        x += "*"
+
+
+triangle()
+
+
+
+
+
+
---
+++ 1-2-1/POD16.py
@@ -0,0 +1,10 @@
+# take in a value from a user
+
+input = input("Give me a value")
+#Add an exclamation to the end
+input += "!"
+for repetition in range(201):
+    print(input)
+
+
+
