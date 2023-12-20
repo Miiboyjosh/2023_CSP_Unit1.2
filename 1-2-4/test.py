e26d4f637a2475664d29ee7b5c15bd2b1e22e6f2
Joshua <joshua.jean@vtsd.com>
2023-12-20 09:24:09 -0500

    1-2-2

--- 1-2-4/test.py
+++ 1-2-4/test.py
@@ -1,35 +1,52 @@
 import turtle as trtl
+import random as rand
 '''
 x = starting distance
 y = incremental distance
 Put it in a loop buddy: (for lines in range 25)
-1. go forward
-2. turn (left 90 degrees)
-3. go forward (x+y)
-2. turn (left 90 degrees)
+    1. go forward
+    2. turn (left 90 degrees)
+    3. go forward (x+y)
+    2. turn (left 90 degrees)
+        add the the draw door function
 
-Doors - 
+
 1. 
 
  
 '''
 # --Configuration--
 maze = trtl.Turtle()
-x = 20
-y = 8
+door_width = 10
+wall_length = 25
+wall_increment = 10
+iteration = 0
 def drawSpiral():
-    global x, y
+    global wall_length, wall_increment, iteration
+    maze.goto(0, 0)
+    maze.speed(0)
     for line in range(25):
-        maze.width(5)
-        maze.left(90)
-        maze.forward(x)
-        x += y
+        if iteration > 3:
+            maze.penup()
+            maze.width(5)
+            maze.left(90)
+            drawDoor()
+            maze.forward(wall_length + wall_increment - 10 - door_width * 2)
+            wall_length += wall_increment
+        iteration += 1
     maze.hideturtle()
-
 def drawDoor():
     maze.forward(10)
     maze.penup()
-    maze.forward(path_width*2)
+    maze.forward(door_width * 2)
+    maze.pendown()
+    maze.forward(40)
+    maze.left(90)
+    maze.forward(door_width * 2)
+    maze.back(door_width * 2)
+    maze.right(90)
+
+
 drawSpiral()
 
 wn = trtl.Screen()
